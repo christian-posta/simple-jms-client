69288f000609d8db01b5c2b754b6b0eed8a9e1a4
Christian Posta <christian.posta@gmail.com>
2014-05-14 10:56:31 -0400

    added some logging to knw when we got a msg

--- src/main/java/org/apache/activemq/Client.java
+++ src/main/java/org/apache/activemq/Client.java
@@ -1,6 +1,10 @@
 package org.apache.activemq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.jms.*;
+import javax.jms.Message;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,16 +14,24 @@ public class Client {
 
     private static String brokerUrl = System.getProperty("brokerUrl", "tcp://localhost:61616");
 
+    private static final Logger LOG = LoggerFactory.getLogger(Client.class);
+
     public static void main(String[] args) throws Exception {
         ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(getBrokerUrl());
 
 
         Connection connection = cf.createConnection();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
         Destination dest = session.createQueue("FOO.QUEUE");
         MessageConsumer consumer = session.createConsumer(dest);
 
-        TimeUnit.SECONDS.sleep(10);
+        Message msg = consumer.receive(TimeUnit.SECONDS.toMillis(10));
+
+        if (msg != null) {
+            LOG.info("WooHoo got a message!");
+        }else {
+            LOG.info(":( boo... no message");
+        }
 
         consumer.close();
         connection.close();
